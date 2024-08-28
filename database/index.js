f1c020eb3f5b7ef420c8331bfbdbc82b3799a226
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2024-08-28 23:56:40 +0530

    DB setup

--- database/index.js
+++ database/index.js
@@ -4,7 +4,8 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME
+    database: process.env.DB_DBNAME,
+    port: 3306
 });
 
 pool.getConnection((err, conn) =>{
