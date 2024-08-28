44051745e2c0de00f02d25f577085df1137739d8
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2024-08-29 00:05:31 +0530

    DB setup

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2')
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || '130.162.54.212', // Fallback to a default IP if DB_HOST is not set
+    host: '130.162.54.212', // Fallback to a default IP if DB_HOST is not set
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DBNAME,
